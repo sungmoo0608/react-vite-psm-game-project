a9a8644295449d30241d5777fbb6357af814e843
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-11 10:20:04 +0900

    기본 파일 정리

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="text-center">
+        <hr />
+        <p>ⓒ Company 2022-2023</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
