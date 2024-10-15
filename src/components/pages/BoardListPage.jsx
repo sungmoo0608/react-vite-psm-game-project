098564b205715ec77f060aed85430c1e9f967c9f
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-15 10:56:25 +0900

    BoardListPage 10개 구현 완료

--- src/components/pages/BoardListPage.jsx
+++ src/components/pages/BoardListPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import boardService from "../../services/BoardService";
+import { Link } from "react-router-dom";
 
 const BoardListPage = () => {
   const [boards, setBoards] = useState([]);
@@ -25,12 +26,99 @@ const BoardListPage = () => {
   }, []);
 
   const initBoards = () => {
-    boardService.getPagingList().then((response) => {
-      console.log(response);
-    });
+    boardService
+      .getPagingList()
+      .then((response) => {
+        console.log(response);
+        setBoards(response.data.boards);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
-  return <div>게시판 실행</div>;
+  return (
+    <div className="container mt-3">
+      <div className="container-fluid">
+        <h1 className="h3 mb-2 text-gray-800">게시판</h1>
+        <p className="mb-4">
+          DataTables is a third party plugin that is used to generate the demo
+          table below. For more information about DataTables, please visit the{" "}
+          <a target="_blank" href="https://datatables.net">
+            official DataTables documentation
+          </a>
+          .
+        </p>
+
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">
+              DataTables Example
+            </h6>
+          </div>
+          <div className="card-body">
+            <div className="table-responsive">
+              <table
+                className="table table-bordered"
+                id="dataTable"
+                width="100%"
+                cellspacing="0"
+              >
+                <thead>
+                  <tr>
+                    <th>번호</th>
+                    <th>이름</th>
+                    <th>제목</th>
+                    <th>날짜</th>
+                    <th>히트</th>
+                    <th className="text-center">삭제</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {boards &&
+                    boards.map((board) => (
+                      <tr key={board.bid}>
+                        <td>{board.bid}</td>
+                        <td>{board.bname}</td>
+
+                        <td>
+                          <Link to={"/board/" + board.bid}>{board.btitle}</Link>
+                        </td>
+
+                        <td>{board.bdate}</td>
+                        <td>{board.bhit}</td>
+                        <td className="text-center">
+                          <button
+                            className="btn btn-success"
+                            value={board.bid}
+                            // onClick={deleteBoard}
+                          >
+                            삭제
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+            {/* 페이징           */}
+            {/* <PaginationB5
+              paging={paging}
+              onClickPaging={onClickPaging}
+            ></PaginationB5> */}
+            <hr />
+            <Link to="/boards/write">
+              <button type="button" className="btn btn-primary">
+                글쓰기
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    // <!-- /.container-fluid -->
+  );
 };
 
 export default BoardListPage;
