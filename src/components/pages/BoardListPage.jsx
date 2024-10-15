5159ab7ca8587c31752722897f8be9f511c6ee9f
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-15 10:33:43 +0900

    BoardListPage 업로드

---
+++ src/components/pages/BoardListPage.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import boardService from "../../services/BoardService";
+
+const BoardListPage = () => {
+  const [boards, setBoards] = useState([]);
+
+  // 정리하면 아래와 같다.
+
+  // useEffect(() => {
+  // 매 렌더링마다 실행
+  // });
+
+  // useEffect(() => {
+  // 컴포넌트가 처음 렌더링된 실행
+  // }, []);
+
+  // useEffect(() => {
+  // 컴포넌트가 처음 렌더링된 이후 실행
+  // a나 b가 변경되어 컴포넌트가 재렌더링된 이후 실행
+  // }, [a, b]);
+
+  useEffect(() => {
+    console.log("use Effective 실행");
+    initBoards();
+  }, []);
+
+  const initBoards = () => {
+    boardService.getPagingList().then((response) => {
+      console.log(response);
+    });
+  };
+
+  return;
+  <div></div>;
+};
+
+export default BoardListPage;
