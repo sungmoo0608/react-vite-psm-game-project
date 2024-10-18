d00b6797f213c62594e2c2b93c3e3a18ec1d9a80
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-18 09:58:34 +0900

    페이징네이션-기본세팅3

--- src/components/pages/BoardListPage.jsx
+++ src/components/pages/BoardListPage.jsx
@@ -6,6 +6,10 @@ import Pagingnation from "../board/Pagingnation";
 const BoardListPage = () => {
   const [boards, setBoards] = useState([]);
 
+  //http://192.168.0.10:8282/boards/list
+
+  const [paging, setPaging] = useState([]);
+
   // 정리하면 아래와 같다.
 
   // useEffect(() => {
