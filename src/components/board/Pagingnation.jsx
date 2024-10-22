abc3b0e09bbc3fc0621d989a1abd99a1d81bacb9
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-22 10:10:50 +0900

    페이징네이션 완성

--- src/components/board/Pagingnation.jsx
+++ src/components/board/Pagingnation.jsx
@@ -1,36 +1,103 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //https://getbootstrap.com/docs/5.0/components/pagination/
 
-const Pagingnation = () => {
+const Pagingnation = ({ paging, onClickPaging }) => {
+  console.log(paging);
+
+  const startQuery =
+    "/boards/list" +
+    "?pageNum=" +
+    (paging.startPage - 1) +
+    "&" +
+    "amount=" +
+    paging.criteria.amount;
+
+  const endQuery =
+    "/boards/list" +
+    "?pageNum=" +
+    (paging.endPage + 1) +
+    "&" +
+    "amount=" +
+    paging.criteria.amount;
+
+  console.log(startQuery);
+
+  const rendering = () => {
+    const row = [];
+
+    for (let i = paging.startPage; i <= paging.endPage; i++) {
+      console.log(
+        "/list/" + "?pageNum=" + i + "&" + "amount=" + paging.criteria.amount
+      );
+
+      const query =
+        "/boards/list" +
+        "?pageNum=" +
+        i +
+        "&" +
+        "amount=" +
+        paging.criteria.amount;
+
+      row.push(
+        <li className="page-item">
+          <Link to={query} onClick={onClickPaging} className="page-link">
+            {i}
+          </Link>
+        </li>
+      );
+    }
+
+    return row;
+  };
+
   return (
     <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">
+      <ul className="pagination">
+        {paging.prev == true && (
+          <li className="page-item">
+            <Link
+              to={startQuery}
+              onClick={onClickPaging}
+              className="page-link"
+              aria-label="Previous"
+            >
+              &laquo;
+            </Link>
+          </li>
+        )}
+
+        {rendering()}
+        {/* <li className="page-item">
+          <a className="page-link" href="#">
             1
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#">
+        <li className="page-item">
+          <a className="page-link" href="#">
             2
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#">
+        <li className="page-item">
+          <a className="page-link" href="#">
             3
           </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
+        </li> */}
+
+        {paging.next == true && paging.endPage > 0 && (
+          <li className="page-item">
+            <Link
+              to={endQuery}
+              onClick={onClickPaging}
+              className="page-link"
+              aria-label="Next"
+            >
+              &raquo;
+              {/* <span aria-hidden="true">&raquo;</span> */}
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
--- src/components/pages/BoardListPage.jsx
+++ src/components/pages/BoardListPage.jsx
@@ -5,10 +5,8 @@ import Pagingnation from "../board/Pagingnation";
 
 const BoardListPage = () => {
   const [boards, setBoards] = useState([]);
-
   //http://192.168.0.10:8282/boards/list
-
-  const [paging, setPaging] = useState([]);
+  const [paging, setPaging] = useState(null);
 
   // 정리하면 아래와 같다.
 
@@ -36,6 +34,9 @@ const BoardListPage = () => {
       .then((response) => {
         console.log(response);
         setBoards(response.data.boards);
+        setPaging(response.data.page);
+
+        console.log(response.data.page);
       })
       .catch((e) => {
         console.log(e);
@@ -45,17 +46,33 @@ const BoardListPage = () => {
   const deleteBoard = (e) => {
     const { name, value } = e.target;
     console.log(name + "::" + value);
+
     boardService
       .remove(value)
-      .then((response) => {
-        console.log(response);
+      .then((respose) => {
+        console.log(respose);
         initBoards();
       })
       .catch((e) => {
         console.log(e);
       });
+  };
 
-    initBoards();
+  const onClickPaging = (e) => {
+    e.preventDefault(); //기존에 링크 동작을 하지 말아라
+
+    console.log(e.target.pathname);
+    console.log(e.target.search);
+
+    boardService
+      .getPagingList(e.target.pathname, e.target.search)
+      .then((response) => {
+        setBoards(response.data.boards);
+        setPaging(response.data.page);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -71,6 +88,7 @@ const BoardListPage = () => {
           .
         </p>
 
+        {/* <!-- DataTales Example --> */}
         <div className="card shadow mb-4">
           <div className="card-header py-3">
             <h6 className="m-0 font-weight-bold text-primary">
@@ -113,7 +131,7 @@ const BoardListPage = () => {
                         <td>{board.bhit}</td>
                         <td className="text-center">
                           <button
-                            className="btn btn-success"
+                            className="btn btn-danger"
                             value={board.bid}
                             onClick={deleteBoard}
                           >
@@ -125,10 +143,13 @@ const BoardListPage = () => {
                 </tbody>
               </table>
             </div>
-
-            {/* 페이징 */}
-            {<Pagingnation></Pagingnation>}
-
+            {/* 페이징           */}
+            {paging != null ? (
+              <Pagingnation
+                paging={paging}
+                onClickPaging={onClickPaging}
+              ></Pagingnation>
+            ) : null}
             <hr />
             <Link to="/boards/write">
               <button type="button" className="btn btn-primary">
@@ -139,7 +160,7 @@ const BoardListPage = () => {
         </div>
       </div>
     </div>
-    // <!-- /.container-fluid -->
+    // <!-- /.container-fluid -->);
   );
 };
 
